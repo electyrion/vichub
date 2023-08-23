c9cfeec0b2ad7784f3378a8a37157160638961db
Vicky Maulana <muhammad.vicky@ui.ac.id>
2023-08-23 07:52:13 +0700

    revert import
    '
    
    '
    '

--- lib/fileMenu/fileMenu.dart
+++ lib/fileMenu/fileMenu.dart
@@ -5,20 +5,15 @@ import 'dart:math' as math;
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
 import 'package:intl/intl.dart';
 import 'package:mac_dt/system/openApps.dart';
 import 'package:mac_dt/sizes.dart';
 import 'package:provider/provider.dart';
 
-import 'package:mac_dt/components/alertDialog.dart';
-import 'package:mac_dt/system/componentsOnOff.dart';
-import 'package:mac_dt/system/folders/folders.dart';
-
-// import '../components/alertDialog.dart';
-// import '../system/componentsOnOff.dart';
-// import '../system/folders/folders.dart';
+import '../components/alertDialog.dart';
+import '../system/componentsOnOff.dart';
+import '../system/folders/folders.dart';
 
 class FileMenu extends StatefulWidget {
 
